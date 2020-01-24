284dd9e6c580c2551a4a05e9c193cfca81de7846
hwstovall <hwstovall@gmail.com>
2020-01-24 17:15:49 -0500

    Update build process. Shrink mobile margins.

--- webpack.config.dev.ts
+++ webpack.config.dev.ts
@@ -57,24 +57,6 @@ const config: webpack.Configuration = {
           },
         ],
       },
-      {
-        test: /\.(png|svg|jpg|ico|gif)$/,
-          use: [
-            'file-loader',
-          ],
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
-      },
     ],
   },
   plugins: [
